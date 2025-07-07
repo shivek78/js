992227e7dec721ec8dd88a0d244d1758462fe5d4
shivek78 <ggi2022.1575@ggi.ac.in>
2025-07-07 13:17:09 +0530

    mouse event

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
- 
+ let button= document.createElement('button')
+  button.className===button
+ button.innerText='2nd button'
+
+ button.onclick=()=>{
+  console.log("mujAH PER CLICK KIYA GAYA OR MAIN JS SA BANA HOON")}
+
+ let body=document.querySelector('body')
+  body.append(button)
+
+ button.addEventListener('click',function(){
+  console.log("mujh per click kiya gaya or main add events");
+ })
+
+ button.addEventListener('click',function(){
+  console.log("mujh per click kiya gaya or main2nd copy hoon or main over side nhi kiya hhai add events");
+ })
+
+ button.addEventListener('click',function(){
+  console.log("mujh per click kiya gaya  3rd hoon or main add events");
+ })
