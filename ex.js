6f52fffc7928e3c374663c5485ffaaba2eec159b
shivek78 <shivekyadav0786@gmail.com>
2025-07-25 13:34:28 +0530

    exaple and learing json

---
+++ ex.js
@@ -0,0 +1,71 @@
+let jsonData = [
+ data={
+    "name": "Mansa Kumar Lal",
+    "age": 21,
+    "isMarried": true,
+    "hobbies": ["coding", "gaming", "reading"],
+    "address": {
+      "street": "123 Tech Lane",
+      "city": "Ludhiana",
+      "pincode": 141001
+    },
+    "children": null,
+    "joiningDate": "2023-06-12"
+  },
+  data1={
+    "name": "Neha Sharma",
+    "age": 26,
+    "isMarried": false,
+    "hobbies": ["design", "music"],
+    "address": {
+      "street": "55 Rainbow Ave",
+      "city": "Chandigarh",
+      "pincode": 160036
+    },
+    "children": [],
+    "joiningDate": "2022-03-21"
+  },
+ data3={
+    "name": "Aditya Das",
+    "age": 29,
+    "isMarried": true,
+    "hobbies": ["traveling", "photography", "cycling"],
+    "address": {
+      "street": "7 Horizon Street",
+      "city": "Delhi",
+      "pincode": 110001
+    },
+    "children": [
+      { "name": "Aarav", "age": 3 },
+      { "name": "Anaya", "age": 1 }
+    ],
+    "joiningDate": "2021-11-05"
+  },
+data4 ={
+    "name": "Tanya Singh",
+    "age": 22,
+    "isMarried": false,
+    "hobbies": null,
+    "address": {
+      "street": "404 Pixel Blvd",
+      "city": "Amritsar",
+      "pincode": 143001
+    },
+    "children": null,
+    "joiningDate": "2024-02-18"
+  },
+data5={
+    "name": "Kabir Bhalla",
+    "age": 24,
+    "isMarried": true,
+    "hobbies": ["blogging", "UI design"],
+    "address": {
+      "street": "22 Flex Grid",
+      "city": "Mumbai",
+      "pincode": 400001
+    },
+    "children": [],
+    "joiningDate": "2023-09-10"
+  }
+
+]
